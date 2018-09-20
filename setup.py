8a491ec682ec43c6baf66f4c30eedb0129de583a
jon <stricoff92@gmail.com>
2018-09-19 23:00:48 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='adstxt',
+    version='0.9',
+    author='Jonathan Stricoff',
+    classifiers=[
+        'Development Status :: 1 - Planning'
+    ]
+    install_requires=[
+
+    ]
+)
